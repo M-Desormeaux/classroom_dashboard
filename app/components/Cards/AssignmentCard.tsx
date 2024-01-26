dc8c1dbbd0700029ef156f54cae776a76222ee06
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-01-26 17:14:32 -0600

    update Student and Assignment cards

--- app/components/Cards/AssignmentCard.tsx
+++ app/components/Cards/AssignmentCard.tsx
@@ -1,6 +1,7 @@
 import { Assignment } from "~/types";
 import { External } from "../Icon";
 import { formatGrade } from "~/utils/formatGrade";
+import { Link } from "@remix-run/react";
 
 export const AssignmentCard = ({
   label = "",
@@ -12,45 +13,47 @@ export const AssignmentCard = ({
   assignmentID,
 }: Assignment) => {
   return (
-    <div className="flex w-full flex-col gap-1 rounded-md bg-gray-100 px-4 py-2 sm:flex-row sm:items-start sm:justify-between sm:gap-3">
-      <div className="flex w-full flex-col sm:w-max">
-        {classID && <span className="text-xs">{classID}</span>}
-        <a
-          href={`/assignments/${assignmentID}`}
-          className="flex items-baseline gap-2 py-2 pr-4 sm:w-max"
-        >
-          <h2 className="text-wrap text-xl font-semibold">
-            Assignment: {label}
-          </h2>
-          <div>
-            <External />
+    <Link
+      to={`/assignments/${assignmentID}`}
+      className="rounded-md bg-gray-100 px-4 py-2 shadow hover:bg-white hover:shadow-md"
+    >
+      <div className="flex w-full flex-col gap-1 sm:flex-row sm:items-start sm:justify-between sm:gap-3">
+        <div className="flex w-full flex-col gap-1 sm:w-max">
+          {classID && <span className="text-xs text-gray-600">{classID}</span>}
+          <div className="flex items-baseline gap-2 sm:w-max ">
+            <h2 className="text-wrap text-xl font-semibold">
+              Assignment: {label}
+            </h2>
+            <div>
+              <External />
+            </div>
           </div>
-        </a>
-        {desc && <span className="line-clamp-2">{desc}</span>}
+          {desc && <span className="line-clamp-2">{desc}</span>}
+        </div>
+        <div className="flex w-full items-end sm:w-max sm:flex-col">
+          <div className="hidden items-center justify-between sm:flex sm:gap-2">
+            <span>High</span>
+            <div className="flex min-w-12 items-center justify-end">
+              <span>{formatGrade(high || 0)}</span>
+              <span>%</span>
+            </div>
+          </div>
+          <div className="flex w-full items-center justify-between sm:gap-2">
+            <span className="text-lg">Average</span>
+            <div className="flex min-w-12 items-center justify-end">
+              <span className="text-xl font-semibold">{formatGrade(avg)}</span>
+              <span>%</span>
+            </div>
+          </div>
+          <div className="hidden items-center justify-between sm:flex sm:gap-2">
+            <span>Low</span>
+            <div className="flex min-w-12 items-center justify-end">
+              <span>{formatGrade(low || 0)}</span>
+              <span>%</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex w-full items-end sm:w-max sm:flex-col">
-        <div className="hidden items-center justify-between sm:flex sm:gap-2">
-          <span>High</span>
-          <div className="flex min-w-12 items-center justify-end">
-            <span>{formatGrade(high || 0)}</span>
-            <span>%</span>
-          </div>
-        </div>
-        <div className="flex w-full items-center justify-between sm:gap-2">
-          <span className="text-lg">Average</span>
-          <div className="flex min-w-12 items-center justify-end">
-            <span className="text-xl font-semibold">{formatGrade(avg)}</span>
-            <span>%</span>
-          </div>
-        </div>
-        <div className="hidden items-center justify-between sm:flex sm:gap-2">
-          <span>Low</span>
-          <div className="flex min-w-12 items-center justify-end">
-            <span>{formatGrade(low || 0)}</span>
-            <span>%</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
--- app/components/Cards/StudentCard.tsx
+++ app/components/Cards/StudentCard.tsx
@@ -1,29 +1,34 @@
 import { Student } from "~/types";
 import { External } from "../Icon";
+import { Link } from "@remix-run/react";
 
 export const StudentCard = ({ studentID, name, avg }: Student) => {
   return (
-    <div className="flex w-full flex-col gap-1 rounded-md bg-gray-100 px-4 py-2 sm:flex-row sm:justify-between">
-      <div className="flex w-full items-center gap-2">
-        <div className="aspect-square w-14 rounded-full bg-gray-400" />
-        <div className="flex flex-col gap-1">
-          <h2 className="text-xl font-semibold">{name}</h2>
-          <span className="flex items-center justify-between gap-1 sm:w-16">
-            Avg:
-            <span>
-              <span className="text-xl">{avg}</span>%
-            </span>
-          </span>
+    <Link
+      to={`/students/${studentID}`}
+      className="rounded-md bg-gray-100 px-4 py-2 shadow hover:bg-white hover:shadow-md"
+    >
+      <div className="flex w-full flex-col justify-end gap-1 sm:flex-row sm:justify-between">
+        <div className="flex w-full flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex items-center gap-2">
+            <div className="h-14 w-14 rounded-full bg-gray-400" />
+            <div className="flex flex-col gap-1">
+              <h2 className="text-xl font-semibold">{name}</h2>
+              <span className="flex w-16 items-center justify-between gap-1">
+                Avg:
+                <span>
+                  <span className="text-xl font-semibold">{avg}</span>%
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-end">
+          <div className="flex w-max items-baseline gap-2 px-4 py-2">
+            See Grades <External />
+          </div>
         </div>
       </div>
-      <div className="flex items-center">
-        <a
-          href={`/student/${studentID}`}
-          className="flex w-max items-baseline gap-2 px-4 py-2"
-        >
-          See Grades <External />
-        </a>
-      </div>
-    </div>
+    </Link>
   );
 };
