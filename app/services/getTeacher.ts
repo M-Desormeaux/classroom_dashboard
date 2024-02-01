b4d8cf84061bef5b8d14f87ee576b91b50e3e6eb
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-02-01 16:09:31 -0600

    prune dead points

--- app/services/getTeacher.ts
+++
@@ -1,7 +0,0 @@
-import { TeachersData } from "./_data";
-
-export const getTeacher = (teacherID: string) => {
-  const teacher = TeachersData.find((d) => d.teacherID === teacherID);
-
-  return teacher;
-};
--- app/services/index.ts
+++ app/services/index.ts
@@ -1,7 +1,5 @@
 export * from "./getAssignment";
 export * from "./getAssignments";
 export * from "./getClasses";
-export * from "./getGrades";
 export * from "./getStudent";
 export * from "./getStudents";
-export * from "./getTeacher";
