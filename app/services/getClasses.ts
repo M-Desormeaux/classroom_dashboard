33dd5e0ed41d5491dab5e79f8c2e5dc4610a7bdf
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-02-01 15:12:43 -0600

    improve get classes service

--- app/services/getClasses.ts
+++ app/services/getClasses.ts
@@ -9,17 +9,17 @@ export const getClasses = async () => {
   if (error) throw new Error();
 
   const temp = data.map((point) => {
-    const grades = point.grades.map((grade) => grade.score);
+    const grades = point.grades.map((grade: { score: number }) => grade.score);
 
-    const filteredGrades = grades.filter((point) => point > 0);
+    const gradeValues = grades.filter((point: number) => point > 0);
     const gradesLength = grades.length;
-    const sum = grades.reduce((acc, curr) => {
+    const sum = grades.reduce((acc: number, curr: number) => {
       return acc + curr;
     }, 0);
     const avg = formatGrade(sum / gradesLength || 0);
 
-    const low = Math.min(...filteredGrades);
-    const high = Math.max(...filteredGrades);
+    const low = Math.min(...gradeValues);
+    const high = Math.max(...gradeValues);
 
     return { ...point, avg, low, high };
   });
